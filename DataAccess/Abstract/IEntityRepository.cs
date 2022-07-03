8ac3a8acb02bef4cb3760b47ae578457db3288ea
gmel <gultekin.melike784@gmail.com>
2022-07-04 00:09:37 +0300

    .

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //class : referans tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabbilir
+    //new() : new'leneilir olmalı
+
+    public interface IEntityRepository<T> where T : class,IEntity
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
